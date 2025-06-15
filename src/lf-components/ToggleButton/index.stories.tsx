08da2d17ac124857be69b06f0d15b312a41cdf6f
TakedaRyota <tkdryt05@gmail.com>
2025-06-15 10:37:32 +0900

    ToggleButton追加

---
+++ src/lf-components/ToggleButton/index.stories.tsx
@@ -0,0 +1,43 @@
+import type { Meta, StoryObj } from '@storybook/nextjs-vite'
+
+import ToggleButton from './index'
+
+const Temp = () => {
+  return (
+    <div className='w-[500px] flex items-center'>
+      <ToggleButton
+        headers={[<div key='0'>DAY1</div>, <div key='1'>DAY2</div>, <div key='2'>DAY3</div>]}
+        contents={[
+          <ToggleButton.Content
+            key='0'
+            className='w-full bg-amber-200 h-[300px]'
+          ></ToggleButton.Content>,
+          <ToggleButton.Content
+            key='1'
+            className='w-full bg-emerald-200 h-[300px]'
+          ></ToggleButton.Content>,
+          <ToggleButton.Content
+            key='2'
+            className='w-full bg-blue-200 h-[300px]'
+          ></ToggleButton.Content>,
+        ]}
+      />
+    </div>
+  )
+}
+
+const meta = {
+  title: 'Components/ToggleButton',
+  component: Temp,
+  parameters: {
+    layout: 'centered',
+  },
+  tags: ['autodocs'],
+} satisfies Meta<typeof Temp>
+
+export default meta
+type Story = StoryObj<typeof meta>
+
+export const Default: Story = {
+  args: {},
+}
---
+++ src/lf-components/ToggleButton/index.tsx
@@ -0,0 +1,69 @@
+import { cn } from '../utils'
+import { ReactNode, useState, PropsWithChildren } from 'react'
+import './toggleButton.css'
+
+const Header = ({ className, children }: PropsWithChildren<ToggleButtonCommon>) => {
+  return <div className={cn('flex flex-col', className)}>{children}</div>
+}
+
+const Content = ({ className, children }: PropsWithChildren<ToggleButtonCommon>) => {
+  return <div className={cn('flex flex-col', className)}>{children}</div>
+}
+
+type ToggleButtonCommon = {
+  className?: string
+}
+
+export type ToggleButtonProps = {
+  Header?: typeof Header
+  Content?: typeof Content
+  headers?: ReactNode[]
+  contents?: ReactNode[]
+  current?: string
+  onChange?: () => void
+}
+
+const ToggleButton = (props: ToggleButtonProps) => {
+  const { headers, contents, current } = props
+  const [tab, setTab] = useState<string>(current ?? '0')
+
+  return (
+    <div className='flex flex-col w-full'>
+      <div className='flex w-11/12 self-center toggle-btn-header rounded-full overflow-hidden my-3'>
+        {headers?.map((header, index) => {
+          return (
+            <button
+              className={cn(
+                'py-2 bg-gray-100',
+                `${index}` === tab && `active-${index}`,
+                `wd-${headers.length}`,
+                index + 1 !== headers.length && 'border-r-gray-400',
+              )}
+              id={`${index}`}
+              onClick={(e) => {
+                setTab(e.currentTarget.id)
+              }}
+              key={index}
+            >
+              {header}
+            </button>
+          )
+        })}
+      </div>
+      <div>
+        {contents?.map((content, index) => {
+          return (
+            <div key={index} className={cn(`${index}` === tab ? 'show' : 'hidden')}>
+              {content}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
+
+ToggleButton.Header = Header
+ToggleButton.Content = Content
+
+export default ToggleButton
