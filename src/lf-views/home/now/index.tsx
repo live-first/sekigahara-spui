311a33207ec65e17a0daf0f4a557976bda0a3d2a
TakedaRyota <tkdryt05@gmail.com>
2025-07-12 18:24:36 +0900

    NOWページの改良

--- src/lf-views/home/now/index.tsx
+++ src/lf-views/home/now/index.tsx
@@ -1,17 +1,22 @@
 'use client'
 
 import { Panel } from '@/lf-components/Panel'
+import ListBox from '@/lf-components/ListBox'
+import { Heading } from '@/lf-components/Heading'
 import { FullModal } from '@/lf-components/FullModal'
 import { FaChevronLeft } from 'react-icons/fa6'
 import './now.css'
+import { StageRow } from '@/lf-templates/stageRow'
 import { Swiper, SwiperSlide } from 'swiper/react'
 import { Autoplay, Navigation, Pagination } from 'swiper/modules'
 import { Image } from '@/lf-components/Image'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
+import { nowPresenter } from './nowPresenter'
 
 export const SekigaharaNow = () => {
+  const { now } = nowPresenter()
 
   const ButtonBanner = () => {
     return (
@@ -62,13 +67,83 @@ export const SekigaharaNow = () => {
       <div className='flex flex-col px-4 my-8 gap-6'>
         {/* <Banner /> */}
         <div className='flex flex-col gap-4'>
-          <div className='flex flex-col gap-1 w-full original-gradient rounded-2xl p-2 text-center'>
-            <div className='text-3xl font-bold text-white text-shadow-lg'>メンテナンス中</div>
-          </div>
+          <div className='text-end'>{now?.datetime}</div>
+          {now?.message && (
+            <div className='flex flex-col gap-2'>
+              <Heading tag={5} label='お知らせ' />
+              <Panel size='lg'>{now?.message}</Panel>
+            </div>
+          )}
+        </div>
+        <div className='flex flex-col gap-2'>
+          <Heading tag={5} label='ステージ進行' />
           <Panel size='lg'>
-            前日に公開予定です。
+            <ListBox>
+              <ListBox.Row>
+                <StageRow
+                  name='徳川ステージ'
+                  src='https://sekigahara-idolwars.net/images/2025/stage/TOKUGAWA_STAGE.png'
+                  status={now?.stage.tokugawa?.status ?? ''}
+                  now={now?.stage.tokugawa?.current ?? ''}
+                  next={now?.stage.tokugawa?.next ?? ''}
+                />
+              </ListBox.Row>
+              <ListBox.Row>
+                <StageRow
+                  name='豊臣ステージ'
+                  src='https://sekigahara-idolwars.net/images/2025/stage/TOYOTOMI_STAGE.png'
+                  status={now?.stage.toyotomi?.status ?? ''}
+                  now={now?.stage.toyotomi?.current ?? ''}
+                  next={now?.stage.toyotomi?.next ?? ''}
+                />
+              </ListBox.Row>
+              <ListBox.Row>
+                <StageRow
+                  name='戦国ステージ'
+                  src='https://sekigahara-idolwars.net/images/2025/stage/SENGOKU_STAGE.png'
+                  status={now?.stage.sengoku?.status ?? ''}
+                  now={now?.stage.sengoku?.current ?? ''}
+                  next={now?.stage.sengoku?.next ?? ''}
+                />
+              </ListBox.Row>
+              <ListBox.Row>
+                <StageRow
+                  name='桃配ステージ'
+                  src='https://sekigahara-idolwars.net/images/2025/stage/MOMOKUBARI_STAGE.png'
+                  status={now?.stage.momokubari?.status ?? ''}
+                  now={now?.stage.momokubari?.current ?? ''}
+                  next={now?.stage.momokubari?.next ?? ''}
+                />
+              </ListBox.Row>
+              <ListBox.Row>
+                <StageRow
+                  name='下剋上ステージ'
+                  src='https://sekigahara-idolwars.net/images/2025/stage/GEGOKUJYO_STAGE.png'
+                  status={now?.stage.gekokujyo?.status ?? ''}
+                  now={now?.stage.gekokujyo?.current ?? ''}
+                  next={now?.stage.gekokujyo?.next ?? ''}
+                />
+              </ListBox.Row>
+            </ListBox>
           </Panel>
         </div>
+        <div className='flex flex-col gap-2'>
+          <Heading tag={5} label='お手洗い' />
+          <Panel size='lg'>
+            <ListBox>
+              <ListBox.Row>♪ 製作中 ♪</ListBox.Row>
+            </ListBox>
+          </Panel>
+        </div>
+        <div className='flex flex-col gap-2'>
+          <Heading tag={5} label='駐車場' />
+          <Panel size='lg'>
+            <ListBox>
+              <ListBox.Row>♪ 製作中 ♪</ListBox.Row>
+            </ListBox>
+          </Panel>
+        </div>
+        <p>実際の状況と多少誤差が生じる可能性がありますが、予めご了承ください。</p>
       </div>
     </FullModal>
   )
--- src/lf-views/home/now/nowPresenter.tsx
+++ src/lf-views/home/now/nowPresenter.tsx
@@ -1,32 +1,43 @@
-import { TimeTableCellType, TimeTableCellType2 } from '@/domain/timetable'
+import { NowResponseType, NowType } from '@/lf-domain/now'
+import { useEffect, useState } from 'react'
 
 export const nowPresenter = () => {
-  const getCurrentArtist = (table: TimeTableCellType[]) => {
-    const now = new Date()
-    const currentMinutes = now.getHours() * 60 + now.getMinutes()
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  const [now, setNow] = useState<NowType>()
 
-    const timetable: TimeTableCellType2[] = table.map((item) => {
-      const startJST = new Date(item.startTime)
-      const endJST = new Date(item.endTime)
-      const startMinutes = startJST.getHours() * 60 + startJST.getMinutes()
-      const endMinutes = endJST.getHours() * 60 + endJST.getMinutes()
-      return {
-        ...item,
-        startMinutes,
-        endMinutes,
-      }
-    })
-
-    const nowIndex: number = timetable.findIndex(
-      (item) => currentMinutes >= item.startMinutes && currentMinutes < item.endMinutes,
+  const getStageStatus = () => {
+    fetch(
+      'https://script.google.com/macros/s/AKfycbwm2XfO0saxuU3EYpyWlHcaTl_ODVRODfvzbKxrlZ7QsiP2XxXkBtRF6Nt3TpV_GPou/exec',
+      { mode: 'cors' },
     )
-
-    return {
-      current: timetable[nowIndex]?.artistName ?? null,
-      next: timetable[nowIndex + 1]?.artistName ?? null,
-      status: 'オンタイム'
-    }
+      .then((response) => response.json())
+      .then((data: NowResponseType) => {
+        const nowData: NowType = {
+          datetime: data.datetime,
+          message: data.message,
+          stage: {
+            tokugawa: data.stage.filter((stage) => stage.name === 'tokugawa')[0],
+            toyotomi: data.stage.filter((stage) => stage.name === 'toyotomi')[0],
+            sengoku: data.stage.filter((stage) => stage.name === 'sengoku')[0],
+            momokubari: data.stage.filter((stage) => stage.name === 'momokubari')[0],
+            gekokujyo: data.stage.filter((stage) => stage.name === 'gekokujyo')[0],
+          },
+          wc: data.wc,
+        }
+        setNow(nowData)
+      })
+      .catch((error) => {
+        console.error('リクエストエラー:', error)
+      })
   }
 
-  return { getCurrentArtist }
+  // eslint-disable-next-line react-hooks/rules-of-hooks
+  useEffect(() => {
+    getStageStatus()
+    const timer = setInterval(getStageStatus, 60000)
+
+    return () => clearInterval(timer)
+  }, [])
+
+  return { now }
 }
