34ff10b0d9c8523488804c4f73a6c8116a6bdd3d
TakedaRyota <tkdryt05@gmail.com>
2025-07-12 18:24:36 +0900

    NOWページの改良

--- src/lf-views/home/now/index.tsx
+++ src/lf-views/home/now/index.tsx
@@ -13,49 +13,10 @@ import { Image } from '@/lf-components/Image'
 import 'swiper/css'
 import 'swiper/css/navigation'
 import 'swiper/css/pagination'
-import { useEffect, useState } from 'react'
 import { nowPresenter } from './nowPresenter'
-import { day1Stage1 } from '@/lf-views/timetable/live/day1/stage1'
-import { day1Stage2 } from '@/lf-views/timetable/live/day1/stage2'
-import { day1Stage3 } from '@/lf-views/timetable/live/day1/stage3'
-import { day1Stage4 } from '@/lf-views/timetable/live/day1/stage4'
-import { day1Stage5 } from '@/lf-views/timetable/live/day1/stage5'
-
-type NowType = {
-  datetime: string
-  error?: string
-  stage: {
-    tokugawa: {
-      status: string | null
-      current: string | null
-      next: string | null
-    }
-    toyotomi: {
-      status: string | null
-      current: string | null
-      next: string | null
-    }
-    momokubari: {
-      status: string | null
-      current: string | null
-      next: string | null
-    }
-    sengoku: {
-      status: string | null
-      current: string | null
-      next: string | null
-    }
-    gekokujyo: {
-      status: string | null
-      current: string | null
-      next: string | null
-    }
-  }
-}
 
 export const SekigaharaNow = () => {
-  const [now, setNow] = useState<NowType>()
-  const { getCurrentArtist } = nowPresenter()
+  const { now } = nowPresenter()
 
   const ButtonBanner = () => {
     return (
@@ -97,44 +58,6 @@ export const SekigaharaNow = () => {
     )
   }
 
-  const getStageStatus = () => {
-    const tokugawa = getCurrentArtist(day1Stage1.data)
-    const toyotomi = getCurrentArtist(day1Stage2.data)
-    const sengoku = getCurrentArtist(day1Stage3.data)
-    const momokubari = getCurrentArtist(day1Stage4.data)
-    const gekokkujyo = getCurrentArtist(day1Stage5.data)
-    setNow({
-      datetime: `${new Date()}`,
-      stage: {
-        tokugawa: tokugawa,
-        toyotomi: toyotomi,
-        momokubari: momokubari,
-        sengoku: sengoku,
-        gekokujyo: gekokkujyo,
-      },
-    })
-  }
-
-  useEffect(() => {
-    getStageStatus()
-    const timer = setInterval(getStageStatus, 60000)
-
-    return () => clearInterval(timer)
-  }, [])
-  // setInterval(() => {
-  //   fetch(
-  //     '',
-  //     { mode: 'cors' },
-  //   )
-  //     .then((response) => response.json())
-  //     .then((data) => {
-  //       setNow(data)
-  //     })
-  //     .catch((error) => {
-  //       console.error('リクエストエラー:', error)
-  //     })
-  // }, 60000)
-
   return (
     <FullModal
       button={<ButtonBanner />}
@@ -144,19 +67,15 @@ export const SekigaharaNow = () => {
       <div className='flex flex-col px-4 my-8 gap-6'>
         {/* <Banner /> */}
         <div className='flex flex-col gap-4'>
-          <div className='flex flex-col gap-1 w-full original-gradient rounded-2xl p-2 text-center'>
-            <div className='text-3xl font-bold text-white text-shadow-lg'>デモ表示中</div>
-          </div>
-          <Panel size='lg'>
-            ・ただいまデモ表示中です。
-            <br />
-            ・7/15〜7/18はメンテナンスとなります。
-            <br />
-            ・当日はリアルタイムの情報を掲載します。
-            <br />
-          </Panel>
+          <div className='text-end'>{now?.datetime}</div>
+          {now?.message && (
+            <div className='flex flex-col gap-2'>
+              <Heading tag={5} label='お知らせ' />
+              <Panel size='lg'>{now?.message}</Panel>
+            </div>
+          )}
         </div>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-2'>
           <Heading tag={5} label='ステージ進行' />
           <Panel size='lg'>
             <ListBox>
@@ -164,51 +83,51 @@ export const SekigaharaNow = () => {
                 <StageRow
                   name='徳川ステージ'
                   src='https://sekigahara-idolwars.net/images/2025/stage/TOKUGAWA_STAGE.png'
-                  status={now?.stage.tokugawa.status ?? ''}
-                  now={now?.stage.tokugawa.current ?? ''}
-                  next={now?.stage.tokugawa.next ?? ''}
+                  status={now?.stage.tokugawa?.status ?? ''}
+                  now={now?.stage.tokugawa?.current ?? ''}
+                  next={now?.stage.tokugawa?.next ?? ''}
                 />
               </ListBox.Row>
               <ListBox.Row>
                 <StageRow
                   name='豊臣ステージ'
                   src='https://sekigahara-idolwars.net/images/2025/stage/TOYOTOMI_STAGE.png'
-                  status={now?.stage.toyotomi.status ?? ''}
-                  now={now?.stage.toyotomi.current ?? ''}
-                  next={now?.stage.toyotomi.next ?? ''}
+                  status={now?.stage.toyotomi?.status ?? ''}
+                  now={now?.stage.toyotomi?.current ?? ''}
+                  next={now?.stage.toyotomi?.next ?? ''}
                 />
               </ListBox.Row>
               <ListBox.Row>
                 <StageRow
                   name='戦国ステージ'
                   src='https://sekigahara-idolwars.net/images/2025/stage/SENGOKU_STAGE.png'
-                  status={now?.stage.sengoku.status ?? ''}
-                  now={now?.stage.sengoku.current ?? ''}
-                  next={now?.stage.sengoku.next ?? ''}
+                  status={now?.stage.sengoku?.status ?? ''}
+                  now={now?.stage.sengoku?.current ?? ''}
+                  next={now?.stage.sengoku?.next ?? ''}
                 />
               </ListBox.Row>
               <ListBox.Row>
                 <StageRow
                   name='桃配ステージ'
                   src='https://sekigahara-idolwars.net/images/2025/stage/MOMOKUBARI_STAGE.png'
-                  status={now?.stage.momokubari.status ?? ''}
-                  now={now?.stage.momokubari.current ?? ''}
-                  next={now?.stage.momokubari.next ?? ''}
+                  status={now?.stage.momokubari?.status ?? ''}
+                  now={now?.stage.momokubari?.current ?? ''}
+                  next={now?.stage.momokubari?.next ?? ''}
                 />
               </ListBox.Row>
               <ListBox.Row>
                 <StageRow
                   name='下剋上ステージ'
                   src='https://sekigahara-idolwars.net/images/2025/stage/GEGOKUJYO_STAGE.png'
-                  status={now?.stage.gekokujyo.status ?? ''}
-                  now={now?.stage.gekokujyo.current ?? ''}
-                  next={now?.stage.gekokujyo.next ?? ''}
+                  status={now?.stage.gekokujyo?.status ?? ''}
+                  now={now?.stage.gekokujyo?.current ?? ''}
+                  next={now?.stage.gekokujyo?.next ?? ''}
                 />
               </ListBox.Row>
             </ListBox>
           </Panel>
         </div>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-2'>
           <Heading tag={5} label='お手洗い' />
           <Panel size='lg'>
             <ListBox>
@@ -216,7 +135,7 @@ export const SekigaharaNow = () => {
             </ListBox>
           </Panel>
         </div>
-        <div className='flex flex-col gap-1'>
+        <div className='flex flex-col gap-2'>
           <Heading tag={5} label='駐車場' />
           <Panel size='lg'>
             <ListBox>
@@ -224,6 +143,7 @@ export const SekigaharaNow = () => {
             </ListBox>
           </Panel>
         </div>
+        <p>実際の状況と多少誤差が生じる可能性がありますが、予めご了承ください。</p>
       </div>
     </FullModal>
   )
